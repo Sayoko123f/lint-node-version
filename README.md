c1e322d4f31259ebed0090fd558aa0324743c7d2
owen123f <owen@anasystem.com.tw>
2023-01-09 12:00:15 +0800

    feat: 開發核心

---
+++ README.md
@@ -0,0 +1,24 @@
+# lint-node-version
+## 安裝
+```
+npm install -g @sayoko123f/lint-node-version
+```
+在 `package.json`
+```json
+{
+    "scripts":{
+        "preinstall": "lint:nodeversion",
+        "prestart": "lint:nodeversion"
+    }
+}
+```
+
+## 檢查
+### 第一步
+檢查 `package.json` 是否有設置 `engines.node`，如果沒設置就警告並退出。
+
+### 第二步
+檢查 `node -v` 是否匹配，如果不匹配就警告並退出。
+
+### 第三步
+檢查 `.npmrc` 是否有設置 `engine-strict=true`，如果沒設置就警告並退出。
---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require("fs/promises");
+const chalk = require("chalk");
+const semver = require("semver");
+const path = require("path");
+const process = require("process");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+async function readPackageJson() {
+  const ctx = await fs.readFile(path.join(process.cwd(), "package.json"));
+  const json = JSON.parse(ctx);
+  return json;
+}
+
+async function getPackageJsonEngines() {
+  const json = await readPackageJson();
+  let nodeVer = null;
+  if (json.engines && json.engines.node) {
+    nodeVer = json.engines.node;
+  }
+  if (!nodeVer) {
+    console.log(chalk.red("必須設定 package.json.engines.node 欄位"));
+    process.exit(1);
+  }
+  return nodeVer;
+}
+
+async function getNpmrc() {
+  try {
+    const ctx = await fs.readFile(path.join(process.cwd(), ".npmrc"), {
+      encoding: "utf8",
+    });
+    return ctx;
+  } catch (err) {
+    console.log(chalk.red("必須擁有 .npmrc 檔案"));
+    process.exit(1);
+  }
+}
+
+async function lintNpmrc() {
+  const ctx = await getNpmrc();
+  if (!ctx.includes("engine-strict=true")) {
+    console.log(chalk.red(".npmrc 必須設定 engine-strict=true"));
+    process.exit(1);
+  }
+}
+
+async function getSystemNodeVersion() {
+  const { stdout, stderr } = await exec("node -v");
+  if (stderr) {
+    console.error(chalk.red(stderr));
+    process.exit(1);
+  }
+  return stdout;
+}
+
+async function lintNodeVersion() {
+  const enginesNode = await getPackageJsonEngines();
+  const currentVersion = await getSystemNodeVersion();
+  if (!semver.satisfies(currentVersion, enginesNode)) {
+    console.log(chalk.red("目前 node 版本不匹配 package.json.engines.node"));
+    console.log("目前使用", currentVersion);
+    console.log("必須匹配", enginesNode);
+    process.exit(1);
+  }
+}
+
+async function main() {
+  await lintNodeVersion();
+  await lintNpmrc();
+}
+
+(async () => {
+  await main();
+})();
